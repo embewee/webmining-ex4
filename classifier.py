32952ff6b6838e194e5918a49e958bffb6c4ed9c
Michael <michi.winkler@web.de>
2014-06-18 19:36:24 +0200

    classifier skritp vorangetrieben

--- classifier.py
+++ classifier.py
@@ -6,11 +6,13 @@ import sqlite3
 DATABASE_NAME = "ex4.db"
 TEST_PATH = "u4_test/"
 GLOBAL_INDEX = "globalIndex";
+RELPROB_PATH = "bayes_model/";
 ######################
 
 # FUNKTIONEN 
 
 def classifyWordVectorForClass(c, wordVector):
+	trainedVector = 
 	for word in wordVector:
 		print word
 
@@ -40,11 +42,17 @@ testFiles = os.listdir(TEST_PATH)
 globalIndex = libGeneral.readDictionaryFromDisk(GLOBAL_INDEX)
 
 classes = []
+classProbVectors = {}
+
 cursor = connection.cursor()
 sql = "SELECT DISTINCT CLASS FROM TRAINING;"
 cursor.execute(sql)
 for row in cursor.fetchall():
-	classes.append(row[0])
+	className = row[0]
+	classes.append(className)
+	classProbVector = libGeneral.readDictionaryFromDisk(RELPROB_PATH + className)
+	classProbVectors[className] = classProbVector
+
 
 for testFile in testFiles:
 	wordVector = libGeneral.createWordVector(TEST_PATH + testFile)
