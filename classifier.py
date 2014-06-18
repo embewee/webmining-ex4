10287d8e9b94c60928024e57187664f69f711940
Michael <michi.winkler@web.de>
2014-06-18 19:19:09 +0200

    klassifizierung vorangetrieben

--- classifier.py
+++ classifier.py
@@ -8,37 +8,44 @@ TEST_PATH = "u4_test/"
 GLOBAL_INDEX = "globalIndex";
 ######################
 
-def classifyForClass(c, word):
-	
+# FUNKTIONEN 
 
-def classify(wordList):
-	
-	
-	
-	
+def classifyWordVectorForClass(c, wordVector):
+	for word in wordVector:
+		print word
+
+'''
+Input:  WordVector wort->Hauefigkeite
+Output: Vector mit Wahrscheinlichkeiten der Zugehoerigkeiten zu einer Klasse
+'''
+def classify(wordVector):
+	probs = {}
 	for c in classes:
-		for word in docString:
-			classifyForClass(c, word)
+		prob = classifyWordVectorForClass(c, wordVector)
+		probs[c] = prob
+	return probs
 
 
 
+
+
+##############################################################
+
+# SKRIPT
 
 connection = sqlite3.connect(DATABASE_NAME)
 
 testFiles = os.listdir(TEST_PATH)
 
-classes = []
-
-#for testFile in testFiles:
-#	wordVector = libGeneral.createWordVector(TEST_PATH + testFile)
-#	print wordVector
-
 globalIndex = libGeneral.readDictionaryFromDisk(GLOBAL_INDEX)
 
+classes = []
 cursor = connection.cursor()
 sql = "SELECT DISTINCT CLASS FROM TRAINING;"
 cursor.execute(sql)
 for row in cursor.fetchall():
 	classes.append(row[0])
 
-
+for testFile in testFiles:
+	wordVector = libGeneral.createWordVector(TEST_PATH + testFile)
+	classify(wordVector)
