20b326995ba4212c6dd74d11ee8548f08ba2f7a6
Michael <michi.winkler@web.de>
2014-06-22 19:45:14 +0200

    knn getestet. Ergebniss-Matrix hinzugefuegt

--- test_classifier.py
+++ test_classifier.py
@@ -3,6 +3,7 @@ import os
 import sqlite3
 import bayes_classifier
 import kNN_classifier
+import codecs
 
 ######################
 GLOBAL_INDEX = "globalIndex";
@@ -13,13 +14,34 @@ globalIndex = libGeneral.readDictionaryFromDisk(GLOBAL_INDEX)
 DATABASE_NAME = "ex4.db"
 connection = sqlite3.connect(DATABASE_NAME)
 
+def printMatrix(matrix):
+	for row in matrix:
+		print row
+
 ###################################
 # SKRIPT ##########################
 ###################################
 
 dirList = os.listdir(TRAINING_PATH)
+nrOfClasses = len(dirList)
 
-for realClass in dirList:
+dictClassToIndex = {}
+index = 0
+for className in dirList:
+	dictClassToIndex[className] = index
+	index += 1
+
+# Creates a list containing nrOfClasses lists initialized to 0
+# Matrix [row][col]
+matrix = [[0 for x in xrange(nrOfClasses)] for x in xrange(nrOfClasses)] 
+
+#reverse mapping
+dictIndexToClass = dict(map(lambda item: (item[1],item[0]),dictClassToIndex.items()))
+
+print "Classifying..."
+
+for i in range (0, nrOfClasses):
+	realClass = dictIndexToClass[i]
 	print "Real class name: " + realClass
 	cursor = connection.cursor()
 	values = [realClass,]
@@ -33,8 +55,26 @@ for realClass in dirList:
 		probs = kNN_classifier.classify(wordVector,7)
 		estimatedClass =  probs[0]
 		print "   " + estimatedClass
+		#matrix[estimated][real] 
+		matrix[dictClassToIndex[estimatedClass]][i] += 1
+		printMatrix(matrix) 
 	
 	print "#################"
+
+# Write results to file
+outputFile = codecs.open("CLASSIFIER_TEST_MATRIX.txt", 'w', encoding="utf-8")
+outputFile.write("v estimated class, > real class")
+
+for i in range (0, nrOfClasses) :
+	outputFile.write(str(i) + "=" + unicode(dictIndexToClass[i]) + '\n')
+	
+for row in matrix:
+	line = ""
+	for element in row:
+		line = line + str(element) + ";"
+	outputFile.write(line + '\n')
+
+outputFile.close()
 
 '''
 ######################
